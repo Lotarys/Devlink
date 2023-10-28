267778c8d3ad37c5f16a56d9c67056ca835e50c0
Lotarys <dima_advanced@Mail.ru>
2023-10-28 11:22:08 +0300

    start configure

---
+++ src/main/java/com/lotarys/devlink/DevlinkApplication.java
@@ -0,0 +1,13 @@
+package com.lotarys.devlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lotarys/devlink/DevlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotarys.devlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
